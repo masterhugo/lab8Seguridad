4d11a91d6e67965464b7f1b5fd8238e8f8f333c1
Hugo Alvarez <hugoferneya@hotmail.com>
2016-04-19 16:04:52 -0500

    se agrego seguridad sin buenos mantenimientos

--- src/main/java/edu/eci/pdsw/webappsintro/controller/ShoppingKartBackingBean.java
+++ src/main/java/edu/eci/pdsw/webappsintro/controller/ShoppingKartBackingBean.java
@@ -18,20 +18,76 @@ package edu.eci.pdsw.webappsintro.controller;
 
 import edu.eci.pdsw.stubs.servicesfacadestub.Producto;
 import edu.eci.pdsw.stubs.servicesfacadestub.ServicesFacade;
+import java.util.LinkedList;
+import javax.faces.event.ActionEvent;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+
 
 /**
  *
  * @author hcadavid
  */
+@ManagedBean(name="carritoDeCompras")
+@SessionScoped
 public class ShoppingKartBackingBean {
-    
+    private int cant,total;
+    private List<Total> cantidad;
+    private Producto selectedProducto;
+
+    public ShoppingKartBackingBean() {
+        cantidad = new LinkedList<Total>();
+        total=0;
+        cant=0;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
     
     public List<Producto> getProductos(){
         return ServicesFacade.getInstance().getProductos();
     }
+
+    public int getCant() {
+        return cant;
+    }
+
+    public void setCant(int cant) {
+        this.cant = cant;
+    }
+
+    public List<Total> getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(List<Total> cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Producto getSelectedProducto() {
+        return selectedProducto;
+    }
+
+    public void setSelectedProducto(Producto selectedProducto) {
+        this.selectedProducto = selectedProducto;
+    }
     
+    public void comprar(){
+        //System.out.println("Entro "+cant+" "+selectedProducto.getNombre()+" "+selectedProducto.getId()+" "+selectedProducto.getPrecioEnPesos());
+        cantidad.add(new Total(selectedProducto.getPrecioEnPesos(), cant, (selectedProducto.getPrecioEnPesos()*cant), selectedProducto.getId(), selectedProducto.getNombre()));
+        //System.out.println("Entro "+cant+" "+selectedProducto.getNombre());
+    }
+    public void neto(){
+        for (Total cantidad1 : cantidad) {
+            total+=(cantidad1.getCantidad()*cantidad1.getPrecio());
+        }
+        cantidad.clear();
+    }
     
 }
---
+++ src/main/java/edu/eci/pdsw/webappsintro/controller/Total.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.pdsw.webappsintro.controller;
+
+/**
+ *
+ * @author Hugo Alvarez
+ */
+public class Total {
+    private int precio,cantidad,total,id;
+    private String nombre;
+
+    public Total(int precio, int cantidad, int total, int id, String nombre) {
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.total = total;
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public Total() {
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getTotal() {
+        return precio*cantidad;
+    }
+    
+}
